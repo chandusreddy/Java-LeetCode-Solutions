359e964afa5527b905db3dbcfedf1b168fd6a020
Chandu0005 <70282603+Chandu0005@users.noreply.github.com>
2023-04-10 17:40:45 -0400

    LeetCode - 977 Update

--- LeetCode/src/com/chandu/leetcode/arrays/SquaresOfaSortedArray.java
+++ LeetCode/src/com/chandu/leetcode/arrays/SquaresOfaSortedArray.java
@@ -2,6 +2,7 @@ package com.chandu.leetcode.arrays;
 
 /*Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
 
+ LeetCode : 977 https://leetcode.com/problems/squares-of-a-sorted-array/
  
 
 Example 1:
@@ -23,32 +24,61 @@ public class SquaresOfaSortedArray {
 //        return nums;
 //    }
 
-	public static int[] sortedSquares(int[] nums) {
+	public static void sortedSquares(int[] nums) {
 		int n = nums.length;
 		int k;
 		for (k = 0; k < n; k++) {
-			if (nums[k] >= 0) 
+			if (nums[k] >= 0)
 				break;
 
 			int i = k - 1; // Initial index of first half
-	        int j = k; // Initial index of second half
-	        int ind = 0; // Initial index of temp array
-	 
-		}
-		return nums;
-			
-		}	
-			
-			
-		
-		
+			int j = k; // Initial index of second half
+			int ind = 0; // Initial index of temp numsay
 
-	
+			int[] temp = new int[n];
+			while (i >= 0 && j < n) {
+				if (nums[i] * nums[i] < nums[j] * nums[j]) {
+					temp[ind] = nums[i] * nums[i];
+					i--;
+				} else {
+
+					temp[ind] = nums[j] * nums[j];
+					j++;
+				}
+				ind++;
+			}
+
+			while (i >= 0) {
+				temp[ind++] = nums[i] * nums[i];
+				i--;
+			}
+			while (j < n) {
+				temp[ind++] = nums[j] * nums[j];
+				j++;
+			}
+
+			// copy 'temp' numsay into original numsay
+			for (int x = 0; x < n; x++)
+				nums[x] = temp[x];
+		}
+
+	}
+
 	public static void main(String[] args) {
 
 		int[] nums = { -7, -3, 2, 3, 11 };
+		int n = nums.length;
+		System.out.println("Before sort ");
+		for (int i = 0; i < n; i++) {
+			System.out.print(nums[i] + " ");
+		}
 
-		int[] squared = sortedSquares(nums);
-		System.out.println("Squared values of the array are:" + squared);
+		sortedSquares(nums);
+		System.out.println();
+		System.out.println("After Sorting : Squared values of the array are:");
+		for (int i = 0; i < n; i++) {
+			System.out.print(nums[i] + " ");
+		}
+
 	}
 }
